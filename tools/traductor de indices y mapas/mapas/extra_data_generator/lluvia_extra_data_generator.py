e1c01cac4563e86ff86bec015dd49a35f717c601
horacio <hmk142@hotmail.com>
2016-07-15 09:47:38 -0300

    nada

--- tools/traductor de indices y mapas/mapas/extra_data_generator/lluvia_extra_data_generator.py
+++ tools/traductor de indices y mapas/mapas/extra_data_generator/lluvia_extra_data_generator.py
@@ -6,34 +6,21 @@ destino = open("./extra_data","w")
 
 origen.read(256 + 7)  # saco header
 
-def escribirMapaData (mapa,lluvia):
-	destino.write('"' + str(mapa) + '"' +":")
-	destino.write('{')
-	destino.write('"outdoor"'+":"+str(lluvia))
-	destino.write('}')
-
 cantidad_mapas = struct.unpack('<H', (origen.read(2)))[0] # l1
 
-destino.write('{')
 x = 1
 while (x < cantidad_mapas +1):
 	# numero X = numero de mapa
 	lluvia = struct.unpack('<B', (origen.read(1)))[0]
 	if (lluvia > 0):
 		lluvia = 1
-	escribirMapaData(x,lluvia);
-	destino.write(',')
+	destino.write(str(x)+"="+'"outdoor"'+":"+str(lluvia)+"\n")
 	x = x +1
 
-#Hay algunos mapas que no figura en FK.ind, lleno con mas mapas y los pongo en 0
+#hay menos mapas aca que en la otra carpeta..
 extra = 100
-while (x < cantidad_mapas +extra):
-	escribirMapaData(x,0);
-	destino.write(',')
+while (x < cantidad_mapas + 1 +extra):
+	destino.write(str(x)+"="+'"outdoor"'+":"+"0"+"\n")
 	x = x +1
 
-#ultimo paso sin ,
-escribirMapaData(x,0);
-
-destino.write('}')
 destino.close()
