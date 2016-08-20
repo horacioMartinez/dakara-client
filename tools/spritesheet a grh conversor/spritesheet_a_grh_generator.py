caac20d01fb6bda8c0da1b835fe43cf017c84dc7
horacio <hmk142@hotmail.com>
2016-08-20 01:39:36 -0300

    spritesheets

--- tools/spritesheet a grh conversor/spritesheet_a_grh_generator.py
+++ tools/spritesheet a grh conversor/spritesheet_a_grh_generator.py
@@ -9,15 +9,20 @@ import struct
 import os  
 from pprint import pprint
 
+graficosModificados = []
+
 with open('graficos.json') as data_file:    
     graficos_data = json.load(data_file)
 
+print("Generando graficos_resultantes.json\n")
 for fn in os.listdir('.'):
 	if not os.path.isfile(fn):
 		continue
 	if not fn.endswith(".json"):
 		continue
 	if fn == "graficos.json":
+		continue
+	if fn == "graficos_resultantes.json":
 		continue
 	nuevo_grafico_nombre = fn[0:fn.find('.')] #nombre de la imagen a la que apuntaran los graficos (el atlas) (idem nombre json sin extension)
 	with open(fn) as atlas_file:    
@@ -31,6 +36,7 @@ for fn in os.listdir('.'):
 					continue
 				grh_grafico_number = grh['grafico']
 				if atlas_grafico_number == grh_grafico_number:
+					graficosModificados.append(grh_grafico_number)
 					atlas_offX = atlas['frames'][atlas_grafico]['frame']['x']
 					atlas_offY = atlas['frames'][atlas_grafico]['frame']['y']
 					grh_offX = grh['offX']
@@ -43,7 +49,21 @@ for fn in os.listdir('.'):
 					grh['offY'] = nuevo_offY
 					grh['css'] = grh['grafico']
 					grh['grafico'] = nuevo_grafico_nombre
-					print (grh_grafico_number)
+
+
+
+#Checkear que de los no modificados esten los png originales
+graficosModificados.append(grh_grafico_number)
+for grh in graficos_data:
+	if type(grh) is not dict: #no es una lista (hay algunos que son 0)
+		continue
+	if 'grafico' not in grh:
+		continue
+	grh_grafico_number = grh['grafico']
+	if grh_grafico_number in graficosModificados :
+		continue
+	if not os.path.isfile(str(grh_grafico_number) + '.png'):
+		print( "ERRROR: " + str(grh_grafico_number))
 
 
 destino = open("graficos_resultantes.json","w")
